e0713954773e757052dc3059c9ee9d807fb3f085
Andereh <ronaldherpc@gmail.com>
2022-06-28 16:59:30 -0400

    Nuevas funciones

--- modules.c
+++ modules.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <stdio_ext.h>
 #include <math.h>
 #include "modules.h"
 
 // Aqui van todas las definiciones de funciones
 
-bool hasAChar(char *strStart, char *strEnd)
+// Funciones recurrentes, tendremos que repetir cosas 34 veces
+// asi que no cae mal unas funciones para evitar eso
+
+bool hasAChar(char *strStart, char *strEnd) // Para validar que una 
+											// cadena no tenga caracteres
 {
 	char *ptr = strStart;
 	
@@ -21,7 +24,21 @@ bool hasAChar(char *strStart, char *strEnd)
 	return false;
 }
 
-int validInt()
+bool wantToRepeat() // Para repetir el bucle
+{
+	char opt[10];
+
+	printf("\n\t1. Continuar\n");
+	printf("\tOtro. Volver al menu\n");
+	printf("\nOpcion: ");
+	fgets(opt, 10, stdin);
+
+	return opt[0] == '1';
+}
+
+int validInt() // Te retorna un int. 
+			   // La uso para no validar cada 
+			   // que necesitamos ingresar algo
 {
 	char str[10];
 	int num, i;
@@ -37,45 +54,48 @@ int validInt()
 	return (int)strtol(str, NULL, 10);
 }
 
-void sayHello()  // prueba
-{
-	printf("Hello\n");
-}
+// Fin de las funciones recurrentes
 
 void promedioNotas() // 01. Ronald
 {
 	int i, sumaTotal = 0, nNotas, puntaje, opt; // nNotas: cantidad de notas a evaluar
 	float res; // resultado
 	char str[10];
-	
-	printf("Modo: Promedio de notas\n\n");
-	printf("Notas a evaluar: ");
-	nNotas = validInt();
 
-	while (nNotas <= 1) 
-	{ // Si se introdujeron caracteres
-		printf("\nIngrese al menos 2 notas. Intente de nuevo\n");
-		printf("Notas a evaluar: ");
-		nNotas = validInt();
-	}
-	
-	printf("\n");
-
-	for (i = 0; i < nNotas; ++i) 
+	do 
 	{
-		printf("Nota %d/%d: ", i + 1, nNotas);
-		puntaje = validInt();
-
-		sumaTotal += puntaje;
-	}
-	
-	res = sumaTotal/(float)nNotas;
-	
-	if (res <= 2.5f) printf("Quedo en transcision\n");
-	else if (res < 3) printf("Perdio la materia\n");
-	else printf("Paso la materia\n");
+		system("clear");
 		
-	printf("\nPromedio total: %f\n", res);
+		printf("Modo: Promedio de notas\n\n");
+		printf("Notas a evaluar: "); nNotas = validInt();
+
+		while (nNotas <= 1)
+		{ 
+			printf("\nIngrese al menos 2 notas. Intente de nuevo\n");
+			printf("Notas a evaluar: ");
+			nNotas = validInt();
+		}
+		
+		printf("Ingrese las notas\n\n");
+
+		for (i = 0; i < nNotas; ++i) 
+		{
+			printf("Nota #%d/%d: ", i + 1, nNotas);
+			puntaje = validInt();
+
+			sumaTotal += puntaje;
+		}
+		
+		res = sumaTotal/(float)nNotas;
+		
+		if (res <= 2.5f)  printf("Quedo en transcision\n");
+		else if (res < 3) printf("Perdio la materia\n"); // Asi dice en el PDF
+		else			  printf("\nPaso la materia\n");
+			
+		printf("\nPromedio total: %f\n", res);
+
+	} while (wantToRepeat());
+
 }
 
 void ecuacionCuadratica()
@@ -83,13 +103,15 @@ void ecuacionCuadratica()
 	int a, b, c;
 	float resPos, resNeg;
 
+	do 
+	{
+
+	system("clear");
+
 	printf("Modo: ecuacion cuadratica\n\n");
-	printf("Valor de A: ");
-	a = validInt();
-	printf("Valor de B: ");
-	b = validInt();
-	printf("Valor de C: ");
-	c = validInt();
+	printf("Valor de A: "); a = validInt();
+	printf("Valor de B: "); b = validInt();
+	printf("Valor de C: "); c = validInt();
 
 	resPos = (-b) + sqrt( fabs ( (pow(b, 2) - (4*a*c) ) ));
 	resNeg = (-b) - sqrt( fabs ( (pow(b, 2) - (4*a*c) ) ));
@@ -97,7 +119,9 @@ void ecuacionCuadratica()
 	resPos /= 2*a;
 	resPos /= 2*a;
 
-	printf("Resultado (+): %f\n", resPos);
+	printf("\nResultado (+): %f\n", resPos);
 	printf("Resultado (-): %f\n", resNeg);
+
+	} while (wantToRepeat());
 
 }
--- modules.h
+++ modules.h
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 #ifndef MODULES_H
-#define MODULES_H value
+#define MODULES_H
 
 // Aqui van todos los prototipos de funciones
 
 void sayHello(); // prueba
-				 //
+int validInt();
 void promedioNotas(); // 01. Ronald
+void ecuacionCuadratica(); // 03. Ronald
 
 #endif 
